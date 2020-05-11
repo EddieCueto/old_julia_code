85689260e7f309f6b7e319fe552db9ba77224b36
Eduardo Cueto Mendoza <cueto303@gmail.com>
2020-05-10 22:34:44 -0600

    [ADD] new files for file management, modules and packages

--- 14_Files.jl
+++ 14_Files.jl
@@ -2,6 +2,8 @@ elements = readdlm("elements.txt")
 
 #elements = readdlm("elements.txt",'\t')
 
+println(typeof(elements))
+
 println(elements[1, 1])
 #println(elements[1])
 
@@ -11,6 +13,10 @@ println(typeof(elements[1,1]))
 println(collect(1:9))
 
 m = reshape(collect(1:9),(3,3))
+m1 = reshape(1:9,(3,3))
+
+println(m)
+println(m1)
 
 writedlm("numbers.txt",m,'\t')
 m2 = readdlm("numbers.txt",Int8)
@@ -18,3 +24,9 @@ m2 = readdlm("numbers.txt",Int8)
 elements = readcsv("elements.csv")
 
 elements, header = readcsv("elements.csv"; header=true)
+
+println(elements)
+
+println(header)
+
+println(elements)
